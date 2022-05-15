9cd40786910df91d29a5bf95fe19fc16679303ba
demothreen <demonnn@list.ru>
2022-05-15 17:42:55 +0500

    add Leaf to proj

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,12 @@
 import Vapor
+import Leaf
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  // uncomment to serve files from /Public folder
+  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    // register routes
-    try routes(app)
+  // register routes
+  try routes(app)
+  app.views.use(.leaf)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,11 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
-    }
+  app.get { req in
+    return "It works!"
+  }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
+  app.on(.GET, "hello", "vapor") { req -> String in
+    return "Hello, VAPOR!"
+  }
 }
